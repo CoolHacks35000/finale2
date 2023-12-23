91184974609260ca859b54a46e7321399c0a695e
CoolHacks35000 <59262645+CoolHacks35000@users.noreply.github.com>
2023-12-23 19:17:15 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,4 @@
-class antiCheat {
-        #lastMove;
-        #inRow;
-    constructor() {
-        this.#lastMove = Date.now();
-        this.#inRow = 0;
-        }
-        #getNewTime() {
-            this.#lastMove = Date.now();
-        }
-        checkTimes() {
-            if (Date.now() - this.#lastMove <= 5) {
-                this.#inRow++;
-            } else {
-                this.#inRow = 0;
-            }
-            this.#getNewTime();
-            return (this.#inRow > 10);
-        }
-}
+
 class secureLogs {
     #spawnLogs;
     #verifiedLogs;
